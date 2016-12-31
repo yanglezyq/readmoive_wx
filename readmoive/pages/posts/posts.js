4eeda11f989a0c793cdf53ae692b47c8d0e20408
zhangyuqing <coderzyq@163.com>
2016-12-31 22:20:34 +0800

    增加几个json数据

--- readmoive/pages/posts/posts.js
+++ readmoive/pages/posts/posts.js
@@ -27,6 +27,42 @@ Page({
         reading: "23",
         collection: "78",
         avatar: "/images/avatar/1.png",
+      },
+      {
+        date: "Jap 01 2017",
+        title: "正式霞飞卸妆",
+        imgSrc: "/images/post/crab.png",
+        content: "哈哈哈哈嘎嘎嘎卧槽啊哈哈哈哈哈哈卡萨减肥的；离开家阿莱克斯觉得烦sdkfja;sldkfjsdfasdfasdf",
+        reading: "23",
+        collection: "78",
+        avatar: "/images/avatar/1.png",
+      },
+      {
+        date: "Jap 01 2017",
+        title: "正式霞飞卸妆",
+        imgSrc: "/images/post/crab.png",
+        content: "哈哈哈哈嘎嘎嘎卧槽啊哈哈哈哈哈哈卡萨减肥的；离开家阿莱克斯觉得烦sdkfja;sldkfjsdfasdfasdf",
+        reading: "23",
+        collection: "78",
+        avatar: "/images/avatar/1.png",
+      },
+      {
+        date: "Jap 01 2017",
+        title: "正式霞飞卸妆",
+        imgSrc: "/images/post/crab.png",
+        content: "哈哈哈哈嘎嘎嘎卧槽啊哈哈哈哈哈哈卡萨减肥的；离开家阿莱克斯觉得烦sdkfja;sldkfjsdfasdfasdf",
+        reading: "23",
+        collection: "78",
+        avatar: "/images/avatar/1.png",
+      },
+      {
+        date: "Jap 01 2017",
+        title: "正式霞飞卸妆",
+        imgSrc: "/images/post/crab.png",
+        content: "哈哈哈哈嘎嘎嘎卧槽啊哈哈哈哈哈哈卡萨减肥的；离开家阿莱克斯觉得烦sdkfja;sldkfjsdfasdfasdf",
+        reading: "23",
+        collection: "78",
+        avatar: "/images/avatar/1.png",
       }
     ];
 
