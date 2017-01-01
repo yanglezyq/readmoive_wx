72bcbafda82cbdbcea40f6099566c8ed9c1f9a6a
unknown <>
2017-01-01 15:09:57 +0800

    给列表item配置ID，并响应点击事件 跳到详情界面

--- readmoive/pages/posts/posts.js
+++ readmoive/pages/posts/posts.js
@@ -6,19 +6,40 @@ Page({
   data: {
     condition: true,
   },
+  onItemTap: function (event) {
+    console.log("onItemTap" + event.currentTarget.dataset.postid);
+    wx.navigateTo({
+      url: 'posts-detail/posts-detail',
+      success: function(res){
+        // success
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+    })
+  },
+
+
   procss: function () {
     console.log("process");
   },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
 
-   console.log("postdata: " + postData);
+    console.log("postdata: " + postData);
 
-// this.data.posts_key = postData.localData;
-  this.setData({
+    // this.data.posts_key = postData.localData;
+    this.setData({
       posts_key: postData.postDataList
     });
   },
+
+
+
+
   onReady: function () {
     // 页面渲染完成
   },
