bb0dec0def1e5fc344fe90e3a264e1fe7d0d5bdd
zhangyuqing <coderzyq@163.com>
2017-01-01 12:27:19 +0800

    将json文件移入单独的 js 文件中。

---
+++ readmoive/data/posts_data.js
@@ -0,0 +1,36 @@
+var localData = [
+  {
+    title: "又是蟹肥虾壮时",
+    content: "菊黄蟹正肥，品尝秋之味。徐志摩把“看初花的荻芦”和“到楼外楼吃蟹”并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
+    imgSrc: "/images/post/crab.png",
+    reading: 62,
+    collection: 92,
+    author: "林白衣",
+    date: "Sep 20 2016",
+    avatar: "/images/avatar/4.png",
+  },
+  {
+    title: "又是蟹肥虾壮时",
+    content: "菊黄蟹正肥，品尝秋之味。徐志摩把“看初花的荻芦”和“到楼外楼吃蟹”并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
+    imgSrc: "/images/post/crab.png",
+    reading: 62,
+    collection: 92,
+    author: "林白衣",
+    date: "Sep 20 2016",
+    avatar: "/images/avatar/4.png",
+  },
+  {
+    title: "又是蟹肥虾壮时",
+    content: "菊黄蟹正肥，品尝秋之味。徐志摩把“看初花的荻芦”和“到楼外楼吃蟹”并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
+    imgSrc: "/images/post/crab.png",
+    reading: 62,
+    collection: 92,
+    author: "林白衣",
+    date: "Sep 20 2016",
+    avatar: "/images/avatar/4.png",
+  },
+];
+
+module.exports = {
+    postDataList: localData
+}
--- readmoive/pages/posts/posts.js
+++ readmoive/pages/posts/posts.js
@@ -1,86 +1,34 @@
 // pages/posts/posts.js
+
+var postData = require("../../data/posts_data.js");
+
 Page({
-  data:{
+  data: {
     condition: true,
   },
-  procss: function() {
+  procss: function () {
     console.log("process");
   },
-  onLoad:function(options){
+  onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
 
-    var post_contents = [
-      {
-        date: "Sep 18 2016",
-        title: "正式霞飞卸妆",
-        imgSrc: "/images/post/crab.png",
-        content: "哈哈哈哈嘎嘎嘎卧槽啊哈哈哈哈哈哈卡萨减肥的；离开家阿莱克斯觉得烦sdkfja;sldkfjsdfasdfasdf",
-        reading: "23",
-        collection: "78",
-        avatar: "/images/avatar/1.png",
-      },
-      {
-        date: "Jap 01 2017",
-        title: "正式霞飞卸妆",
-        imgSrc: "/images/post/crab.png",
-        content: "哈哈哈哈嘎嘎嘎卧槽啊哈哈哈哈哈哈卡萨减肥的；离开家阿莱克斯觉得烦sdkfja;sldkfjsdfasdfasdf",
-        reading: "23",
-        collection: "78",
-        avatar: "/images/avatar/1.png",
-      },
-      {
-        date: "Jap 01 2017",
-        title: "正式霞飞卸妆",
-        imgSrc: "/images/post/crab.png",
-        content: "哈哈哈哈嘎嘎嘎卧槽啊哈哈哈哈哈哈卡萨减肥的；离开家阿莱克斯觉得烦sdkfja;sldkfjsdfasdfasdf",
-        reading: "23",
-        collection: "78",
-        avatar: "/images/avatar/1.png",
-      },
-      {
-        date: "Jap 01 2017",
-        title: "正式霞飞卸妆",
-        imgSrc: "/images/post/crab.png",
-        content: "哈哈哈哈嘎嘎嘎卧槽啊哈哈哈哈哈哈卡萨减肥的；离开家阿莱克斯觉得烦sdkfja;sldkfjsdfasdfasdf",
-        reading: "23",
-        collection: "78",
-        avatar: "/images/avatar/1.png",
-      },
-      {
-        date: "Jap 01 2017",
-        title: "正式霞飞卸妆",
-        imgSrc: "/images/post/crab.png",
-        content: "哈哈哈哈嘎嘎嘎卧槽啊哈哈哈哈哈哈卡萨减肥的；离开家阿莱克斯觉得烦sdkfja;sldkfjsdfasdfasdf",
-        reading: "23",
-        collection: "78",
-        avatar: "/images/avatar/1.png",
-      },
-      {
-        date: "Jap 01 2017",
-        title: "正式霞飞卸妆",
-        imgSrc: "/images/post/crab.png",
-        content: "哈哈哈哈嘎嘎嘎卧槽啊哈哈哈哈哈哈卡萨减肥的；离开家阿莱克斯觉得烦sdkfja;sldkfjsdfasdfasdf",
-        reading: "23",
-        collection: "78",
-        avatar: "/images/avatar/1.png",
-      }
-    ];
+   console.log("postdata: " + postData);
 
-
-    this.setData({
-      posts_key: post_contents
+// this.data.posts_key = postData.localData;
+  this.setData({
+      posts_key: postData.postDataList
     });
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
   }
 })
