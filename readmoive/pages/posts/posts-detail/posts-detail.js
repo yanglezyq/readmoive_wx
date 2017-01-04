85dd89f86afe18be47a8342f02f7ff915552099f
zhangyuqing <coderzyq@163.com>
2017-01-04 23:24:55 +0800

    利用 storage 本地缓存，实现 电影收藏功能。

--- readmoive/pages/posts/posts-detail/posts-detail.js
+++ readmoive/pages/posts/posts-detail/posts-detail.js
@@ -6,14 +6,43 @@ Page({
   data:{},
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
-    var postData = postsData.postDataList[options.id];
+    var postId = options.id;
+    this.data.currentPostId = postId;
+    var postData = postsData.postDataList[postId];
 
     this.setData({
       detailModel: postData,
     });
 
+    var posts_collected_key = "posts_collected_key";
 
+
+    var postsCollected = wx.getStorageSync(posts_collected_key);
+    if (postsCollected) {
+        var postCollected = postsCollected[postId];
+        this.setData({
+          collected: postCollected,
+        });
+    } else {
+      postsCollected = {};
+      postsCollected[postId] = false;
+      wx.setStorageSync(posts_collected_key, postsCollected);
+    }
   },
+
+  onCollectionTap: function(event) {
+    var posts_collected_key = "posts_collected_key";
+    var postId = this.data.currentPostId;
+    var postsCollected = wx.getStorageSync(posts_collected_key);
+    var postCollected = postsCollected[postId];
+    postCollected = !postCollected;
+    postsCollected[postId] = postCollected;
+    wx.setStorageSync(posts_collected_key, postsCollected);
+    this.setData({
+      collected: postCollected,
+    });
+  },
+
   onReady:function(){
     // 页面渲染完成
   },
