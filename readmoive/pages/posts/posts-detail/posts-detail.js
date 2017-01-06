e521e17af24988f5459acdc056196174fd637b7e
zhangyuqing <coderzyq@163.com>
2017-01-07 00:01:22 +0800

    音乐信息从 json中加载。

--- readmoive/pages/posts/posts-detail/posts-detail.js
+++ readmoive/pages/posts/posts-detail/posts-detail.js
@@ -54,14 +54,21 @@ Page({
 
 
   onPlayMusicTap: function (event) {
-    var url = "http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46";
-    var title = "此时此刻";
-    var coverImg = "../images/music/xuwei.jpg";
+    // var url = "http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46";
+    // var title = "此时此刻";
+    // var coverImg = "../images/music/xuwei.jpg";
+
+
+    var postId = this.data.currentPostId;
+    var postData = postsData.postDataList[postId];
+
+    var musicVO = postData.music;
+
     if (!this.data.isPlayingMusic) {
       wx.playBackgroundAudio({
-        dataUrl: url,
-        title: title,
-        coverImgUrl: coverImg,
+        dataUrl: musicVO.url,
+        title: musicVO.title,
+        coverImgUrl: musicVO.coverImg,
         success: function (res) {
           // success
         },
